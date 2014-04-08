9de988f541c865d2c7ba622b148aa61d5dbc564f
Chris Cunningham <chris.git@cmcproductions.co.uk>
2014-04-08 11:54:10 -0300

    added error handling

--- scripts/goNvil.py
+++ scripts/goNvil.py
@@ -1,12 +1,17 @@
 import os
 import pymel.core as pm
+import sys
 
 objPath = os.environ['APPDATA']
 objPath = objPath + '/DigitalFossils/NVil/Media/Clipboard/ClipboardObj.obj'
 
 
 def importObj():
-    pm.importFile(objPath)
+    try:
+        pm.importFile(objPath)
+        sys.stderr.write('NvilClipboard Imported!')
+    except:
+        pm.warning('File Not Found!')
 
 
 def exportObj(center=False):
@@ -19,9 +24,11 @@ def exportObj(center=False):
                 oldLoc = obj.getRotatePivot()
                 centerPiv(obj)
                 pm.exportSelected(objPath, pr=True, typ='OBJexport', es=1, force=True, op="groups=1;ptgroups=1;materials=1;smoothing=1;normals=1")
+                sys.stderr.write('NvilClipboard Exported!')
                 obj.translateBy(oldLoc)
         else:
             pm.exportSelected(objPath, pr=True, typ='OBJexport', es=1, force=True, op="groups=1;ptgroups=1;materials=1;smoothing=1;normals=1")
+            sys.stderr.write('NvilClipboard Exported!')
 
 
 def centerPiv(obj):
